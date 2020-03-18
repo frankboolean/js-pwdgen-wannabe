5b93c7e2915249fc5faf9e60a2643223d55f2b9e
Francesco <fra.iozzia@gmail.com>
2020-03-18 17:33:36 +0100

    fine

---
+++ main.js
@@ -0,0 +1,19 @@
+//var ciao=prompt(come ti chiami?)
+
+// commento
+
+/*
+commento
+*/
+
+
+
+var mioNome=prompt("come ti chiami?");
+
+
+var mioCognome=prompt("Qual'è il tuo cognome?")
+
+var mioColore=prompt("Qual'è il tuo colore preferito")
+// io voglio memorizzare nella veriabile che si chiama mioNome il valore dato indietro da prompt
+
+document.getElementById('messaggio').innerHTML="ciao la tua password è " + mioNome +  mioCognome + mioColore+19;
